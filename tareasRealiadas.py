638f586a763ab5f42f95b9eaebb42f6b91d25b8c
Juanfran <pjuanzamora@gmail.com>
2022-01-27 09:01:07 +0100

    Aleatorios random.choice() -- Funcion range() -- Bucle For

--- tareasRealiadas.py
+++ tareasRealiadas.py
@@ -4,35 +4,7 @@
 #Ejercicios 1-5 y 15
 #Try - Except
 #Números aleatorios -- Ejercicio refuerzo 1
+#Números aleatorios 2 -- Funciones en Python range -- Bucle For
+#Ejercicios 5 - 6 - 7 - 8 del pdf Bucles
 
-import random
-
-jugar = "s"
-while jugar == "s":
-    aleatorio = random.randint(0,10)
-    seleccion = -1
-    intentos = 0
-    bandera = True
-    while seleccion != aleatorio:
-
-    #Bloque para leer un número sin fallos
-        while bandera: 
-            try:
-                seleccion = (int)(input("Dame un número "))
-                bandera = False
-            except:
-                print("Un NÚMERO MELÓN")
-    #Fin leer un número sin fallos, pongo la bandera de nuevo a True
-        bandera = True
-    #Doy pista si es mayor o menor
-        if (seleccion < aleatorio):
-            print("El número es mayor")
-            intentos += 1
-        elif (seleccion > aleatorio):
-            print("El número es menor")
-            intentos += 1
-
-    #Fin del programa
-    print("Felicidades! Has adivinado el número ", aleatorio,  "en ", intentos, "intentos")
-    jugar = input("¿Quieres volver a jugar? s/n")
 
