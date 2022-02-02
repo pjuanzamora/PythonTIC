b4ae04683d770129579a9d771b6d5b019d691778
Juanfran <pjuanzamora@gmail.com>
2022-02-02 12:43:53 +0100

    Listas y sus funciones

--- tareasRealiadas.py
+++ tareasRealiadas.py
@@ -6,5 +6,23 @@
 #Números aleatorios -- Ejercicio refuerzo 1
 #Números aleatorios 2 -- Funciones en Python range -- Bucle For
 #Ejercicios 5 - 6 - 7 - 8 del pdf Bucles
+#Listas
+
+
+
+vNum = []
+num = 0
+
+while num != -1:
+    num = int(input("Dame un numero: \n"))
+    if (num != -1):
+        vNum.append(num)
+
+
+print ("Los números leidos son: ", len(vNum), " y son: ", vNum)
+print ("El mayor es: ", max(vNum))
+print ("El menor es: ", min(vNum))
+
+
 
 
